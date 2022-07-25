49b805238d55c5361aa264a908277350424056eb
dangvinhnd99 <dangvinhnd99@gmail.com>
2022-07-25 08:28:54 +0000

    thay title

--- react-student-management-web-app/src/App.js
+++ react-student-management-web-app/src/App.js
@@ -13,7 +13,7 @@ function App() {
       <div>
         <nav className="navbar navbar-expand navbar-dark bg-dark">
           <a href="/students" className="navbar-brand">
-            DaiCaVinh
+            DaiCaVinh-hehe
           </a>
           <div className="navbar-nav mr-auto">
             <li className="nav-item">
